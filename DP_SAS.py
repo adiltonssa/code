dd0ad23b9dd246d4bdf938dd540305cf715026c2
adiltonssa <65089313+adiltonssa@users.noreply.github.com>
2023-02-27 12:41:17 +0100

    Update DP_SAS.py

--- DP_SAS.py
+++ DP_SAS.py
@@ -23,37 +23,8 @@ from plotly.subplots import make_subplots
 
 #image = Image.open('201103_INEOS_POETS_V_CMYK.jpg')
 #st.sidebar.image(image)
-
-
 import streamlit as st
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="POETS"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
 
 FR = st.slider('Flow (kg/h): ',1, 10000,1000)
 st.write('Flow :', FR,'(kg/h)')
