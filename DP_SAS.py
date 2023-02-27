d4eb34c93e83ceb6199c8dd144c5c5c6e6c4e251
adiltonssa <65089313+adiltonssa@users.noreply.github.com>
2023-02-27 14:03:54 +0100

    Update DP_SAS.py

--- DP_SAS.py
+++ DP_SAS.py
@@ -19,7 +19,7 @@ import streamlit as st
 #image = Image.open('201103_INEOS_POETS_V_CMYK.jpg')
 #st.sidebar.image(image)
 
-
+st.header('Delta Pressure Multisteps - SAS Slurry Process INEOS')
 
 FR = st.slider('Flow (kg/h): ',1, 10000,1000)
 st.write('Flow :', FR,'(kg/h)')
