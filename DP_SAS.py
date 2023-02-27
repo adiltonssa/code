b74f29e6bb89832490236b1a3f568342f73e6eb3
adiltonssa <65089313+adiltonssa@users.noreply.github.com>
2023-02-27 16:55:04 +0100

    Update DP_SAS.py

--- DP_SAS.py
+++ DP_SAS.py
@@ -52,7 +52,7 @@ if check_password():
     image = Image.open('201103_INEOS_POETS_V_CMYK.jpg')
     st.sidebar.image(image)
 
-    st.header('Delta Pressure Multisteps - SAS Slurry Process INEOS')
+    st.header('Delta Pressure Multisteps - Slurry Process INEOS')
 
     FR = st.slider('Flow (kg/h): ',1, 10000,1000)
     st.write('Flow :', FR,'(kg/h)')
