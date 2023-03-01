ae103d647ee4e1226ec9fb0d81003ec978bbfdcf
adiltonssa <65089313+adiltonssa@users.noreply.github.com>
2023-03-01 16:02:17 +0100

    Update DP_SAS.py

--- DP_SAS.py
+++ DP_SAS.py
@@ -138,9 +138,10 @@ if dift>0:
     
             inter=inter+1
             crf=1000*(FR1-FR0)/FR1
- st.metric('Flow rate (kg/h) ',"{:0.0f} K".format(FR1)) 
+ st.metric('Flow rate (kg/h) ',"{:0.0f} K".format(FR1))) 
 
 FR=FR1
+
     DP_g=[]
     sp_g=[]
     DP_B=[]
