5bc0f7617469ab091231386df76ba72833669f5c
adiltonssa <65089313+adiltonssa@users.noreply.github.com>
2023-03-06 12:25:03 +0100

    Update DP_SAS.py

--- DP_SAS.py
+++ DP_SAS.py
@@ -49,10 +49,10 @@ def check_password():
 
 def flow(P_inlet,Pf,FR,rou,Visc,di,l_s):
         Pf=P_inlet
-        DP_g=[]
-        sp_g=[]
+        #DP_g=[]
+        #sp_g=[]
         DP_B=[]
-        PF=[]
+        #PF=[]
         rho=Dens*(Pf/P_inlet)**2
         for i in range(len(l_s)):
 
@@ -62,6 +62,7 @@ def flow(P_inlet,Pf,FR,rou,Visc,di,l_s):
             res=Df2(sp,l_s[0][i],Visc,di,rou,Re)
             Pf=(Pf-res*rho*9.81*0.00001)
             rho=Dens*(Pf/P_inlet)**2
+            
             DP_B.append(res*rho*9.81*0.00001)
         
         return sum(DP_B)
@@ -71,7 +72,7 @@ def flow(P_inlet,Pf,FR,rou,Visc,di,l_s):
 def Df2(sp,ls,vis,di,rou,Re):
     
     def diff(pc):
-        diff=100000*((0.0625/((np.log10(rou/di/3.7+5.74/(Re**0.9))))**2)-(pc*(di/1000)*9.81/(2*ls*(sp**2))))    
+        diff=100000*((0.0625/((math.log10(rou/di/3.7+5.74/(Re**0.9))))**2)-(pc*(di/1000)*9.81/(2*ls*(sp**2))))
         return diff
     
     pc = spo.root(diff,0)
