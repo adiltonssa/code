d90a397e0ae553b47bd877bb5cc5f59131517cba
adiltonssa <65089313+adiltonssa@users.noreply.github.com>
2023-03-07 14:47:51 +0100

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -11,9 +11,10 @@ st.set_page_config(page_title="Interface",page_icon="💻", )
 st.write("# Welcome to the future! 👋")
 
 st.sidebar.success("Select the application:")
-st.markdown( "" This initiative aims to migrate applications used by the POETS INEOS engineering team to a more friendly, versatile and agile platform.
+st.markdown(    """
+        This initiative aims to migrate applications used by the POETS INEOS engineering team to a more friendly, versatile and agile platform.
     Everyone's contribution is essential. It is a beta version, thus, if you find any abnormality, please contact the developer.
-    "")
+    """)
 
 
 st.caption('Application developed by Adilton Lopes da Silva (INEOS Polymers Engineering & Technology Support)')
