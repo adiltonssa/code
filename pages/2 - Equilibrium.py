c09067790a7f661fbaf683ec33333c30cc2cb283
adiltonssa <65089313+adiltonssa@users.noreply.github.com>
2023-03-08 13:55:00 +0100

    Update 2 - Equilibrium.py

--- pages/2 - Equilibrium.py
+++ pages/2 - Equilibrium.py
@@ -389,7 +389,7 @@ if opt=='Table':
 elif opt=="Download":
         def convert_df(df):
     
-        return df.to_csv().encode('utf-8')
+                return df.to_csv().encode('utf-8')
 
         csv = convert_df(df_n)
 
