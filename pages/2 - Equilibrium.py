200b303d8d4d2941030122db7abd3d4c29543667
adiltonssa <65089313+adiltonssa@users.noreply.github.com>
2023-03-08 13:56:27 +0100

    Update 2 - Equilibrium.py

--- pages/2 - Equilibrium.py
+++ pages/2 - Equilibrium.py
@@ -393,8 +393,6 @@ elif opt=="Download":
 
         csv = convert_df(df_n)
 
-        st.download_button(label="Download result as CSV",data=csv,
-        file_name='results.csv',mime='text/csv',)
+        st.download_button(label="Download result as CSV",data=csv,file_name='results.csv',mime='text/csv')
         
- else:
-        
+ else:       
